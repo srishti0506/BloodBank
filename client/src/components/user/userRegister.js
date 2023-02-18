3d6a1dac848e44833412729b84753bc700f88796
srishti0506 <78142550+srishti0506@users.noreply.github.com>
2023-02-18 10:05:08 +0530

    Update userRegister.js

--- client/src/components/user/userRegister.js
+++ client/src/components/user/userRegister.js
@@ -40,7 +40,7 @@ const UserRegister = () => {
 
   return (
     <div className="user-register">
-      <h2>DONAR REGISTER</h2>
+      <h2>DONOR REGISTER</h2>
       <form className="userReg-form" onSubmit={(e)=>submituserRegister(e)}>
         <input className="form-control"
           name="userFName"
